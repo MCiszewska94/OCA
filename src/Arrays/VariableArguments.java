99dfe50062a20f7e56c16f8a447bd3ce287ce395
MCiszewska94 <malgorzata.ciszewska94@gmail.com>
2017-12-22 20:16:23 +0100

    Variable arguments lesson

--- src/Arrays/VariableArguments.java
+++ src/Arrays/VariableArguments.java
@@ -1,11 +1,30 @@
 package Arrays;
 
+import java.util.Arrays;
+
 public class VariableArguments {
 
 //    public static void main(String[] args) {}
 
+
     public static void main(String... args){
+        System.out.println(args.length);
+        System.out.println(Arrays.toString(args));
+        System.out.println();
+        print(args);
+        print("Java"); // don't need to pass array to this method, you can pass as many as you like arguments
+        // they just need to be the same type !
+
+
+        // it works because print method has String... argument
+        // if we change String... args to String[] args it won't work!
+        print("this","is","my","string","array");
+        print(new String[]{"this","is","my","string","array"}); // don't need to create an array
+        print("variable","arguments","are","nice","and","useful","feature");
 
     }
 
+    private static void print(String... args){
+        System.out.println(Arrays.toString(args));
+    }
 }
