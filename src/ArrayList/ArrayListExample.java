0550b8a52931dce3bbd5838ec3630798feab78f2
MCiszewska94 <malgorzata.ciszewska94@gmail.com>
2017-12-25 13:53:56 +0100

    ArrayListExample lesson

---
+++ src/ArrayList/ArrayListExample.java
@@ -0,0 +1,20 @@
+package ArrayList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListExample {
+
+    public static void main(String[] args) {
+        ArrayList myList = new ArrayList();
+        ArrayList<Object> myList2 = new ArrayList<>();
+
+
+        ArrayList<String> list = new ArrayList<>();
+
+        // polymorphism
+        List<String> list3 = new ArrayList<>(); // using interface as type because array list class implements list interface
+
+
+    }
+}
